8471adf30c2d7917e06afc0f473c43cb4a5c8b29
Thitari Somboon <72558791+Thitari-Somboon@users.noreply.github.com>
2021-01-12 12:01:22 +0100

    Create new IT project

---
+++ src/main/java/com/ITProjrect/BearBellyBar/BearBellyBarApplication.java
@@ -0,0 +1,13 @@
+package com.ITProjrect.BearBellyBar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BearBellyBarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BearBellyBarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ITProjrect/BearBellyBar/BearBellyBarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ITProjrect.BearBellyBar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BearBellyBarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
