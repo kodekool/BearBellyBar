800b0a505b7f32fb11d8161230b087ce63492176
Thitari-Somboon <thitari.somboon@students.fhnw.ch>
2021-01-12 13:57:02 +0100

    main calss renamed

--- src/main/java/process/bbb/BearBellyBarApplication.java
+++ src/main/java/process/bbb/BearBellyBarApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BbbApplication {
+public class BearBellyBarApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BbbApplication.class, args);
+		SpringApplication.run(BearBellyBarApplication.class, args);
 	}
 
 }
